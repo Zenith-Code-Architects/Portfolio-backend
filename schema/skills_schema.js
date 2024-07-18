ff1c9a30d8f3e496f38036b6e4130d4827f682aa
Busola Tom <busolatom@gmail.com>
2024-07-18 15:27:53 +0000

    skillsModel updated

--- schema/skills_schema.js
+++ schema/skills_schema.js
@@ -2,6 +2,6 @@ import Joi from "joi";
 
 export const skills_schema = Joi.object({
     name: Joi.string().required(),
-    levelOfProfiency: Joi.string(),
+    levelOfProficiency: Joi.string(),
     user: Joi.string()
 })
